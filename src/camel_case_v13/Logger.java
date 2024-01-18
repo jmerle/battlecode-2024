5338b516581e010deed5144d0eccb777a57900da
Jasper van Merle <jaspervmerle@gmail.com>
2024-01-18 20:41:38 +0100

    v13

---
+++ src/camel_case_v13/Logger.java
@@ -0,0 +1,18 @@
+package camel_case_v13;
+
+public class Logger extends Globals {
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void flush() {
+        rc.setIndicatorString(sb.toString());
+        sb = new StringBuilder();
+    }
+
+    public static void log(String message) {
+        if (sb.length() > 0) {
+            sb.append(", ");
+        }
+
+        sb.append(message);
+    }
+}
